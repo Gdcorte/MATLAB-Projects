577c47ef0c11f6e83e6d7a04acfbdf65a6e0fd33
Gustavo DIniz da COrte <gustavodacorte@gmail.com>
2019-11-02 11:00:24 -0300

    feat: ANFIS->update main script

--- adaptative-neuro-fuzzy/main.m
+++ adaptative-neuro-fuzzy/main.m
@@ -10,16 +10,16 @@
 %Contact: gustavodacorte@gmail.com
 
 %Create new ANFIS object
-Nr = 10; %Number of Rules
+Nr = 50; %Number of Rules
 model = anfis(Nr);
 
 %Create training dataset
 m = 100; %Sample ammount
 syms f(x)
-f(x) = x.^2; %Target Function
-% f(x) = sin(x);
-x_0 = -1; %Starting Point
-x_f = 1; %Ending Point
+% f(x) = x.^2; %Target Function
+f(x) = sin(x);
+x_0 = -10; %Starting Point
+x_f = 10; %Ending Point
 x = linspace(x_0,x_f,m);
 y=double(f(x));
 
@@ -27,6 +27,9 @@ y=double(f(x));
 model.trainModelGaussian(x,y);
 
 %Algorithm Testing
+m = m;
+x = linspace(x_0,x_f,m);
+y=double(f(x));
 testing = zeros(1,m);
 for i = 1:1:m %for each data point
         testing(i) = model.runModel(x(i));
@@ -40,4 +43,4 @@ plot(x,testing,'.g');
 xlabel('x');
 ylabel('Output');
 legend('Original Function','Approximate Model');
-title('Sistemas Nebulosos Modelo Adaptativo');
+title('ANFIS approximation');
